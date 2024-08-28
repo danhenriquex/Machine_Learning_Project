677786b2b418e544ac10693b2d22c6293f607eea
danhenriquex <danilohssantana@gmail.com>
2024-08-28 10:51:55 -0300

    added functionality to initialize DVC

---
+++ project_environment/utils/data_utils.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from project_environment.utils import get_logger, run_shell_command
+
+DATA_UTILS_LOGGER = get_logger(Path(__file__).name)
+
+
+def is_dvc_initialized() -> bool:
+    return (Path().cwd() / ".dvc").exists()
+
+
+def initialize_dvc() -> None:
+    if is_dvc_initialized():
+        DATA_UTILS_LOGGER.info("DVC is already initialized")
+        return
+    DATA_UTILS_LOGGER.info("Initializing DVC")
+    run_shell_command("dvc init")
+    run_shell_command("dvc config core.analytics false")
+    run_shell_command("dvc config core.autostage true")
+    run_shell_command("git add .dvc")
+    run_shell_command("git commit -nm 'Initialized DVC'")
--- project_environment/version_data.py
+++ project_environment/version_data.py
@@ -1,11 +1,15 @@
+from pathlib import Path
+
 from project_environment.config_schemas.config_schema import Config
 from project_environment.utils.config_utils import get_config
+from project_environment.utils.data_utils import get_config, initialize_dvc
+from project_environment.utils.utils import get_logger
 
 
 @get_config(config_path="../configs", config_name="config")
-def entrypoint(config: Config) -> None:
-    print(config)
+def version_data(config: Config) -> None:
+    initialize_dvc()
 
 
 if __name__ == "__main__":
-    entrypoint()  # type ignore
+    version_data()  # type ignore
